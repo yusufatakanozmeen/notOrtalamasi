74d0da55484d892e66e854cc380dbad29168bc46
yusufatakanozmeen <107477926+yusufatakanozmeen@users.noreply.github.com>
2022-08-04 15:13:54 +0300

    Add files via upload

---
+++ notOrtalamasi.java
@@ -0,0 +1,22 @@
+public class notOrtalamasi {
+    public static void main(String[] args) {
+        //not ortalamasi
+        int not1 , not2 , not3 , not4 , not5, not6;
+        double ortalama;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Matematik Notu giriniz : ");
+        not1 = input.nextInt();
+        System.out.println("Fizik Notu giriniz : ");
+        not2 = input.nextInt();
+        System.out.println("Kimya Notu giriniz : ");
+        not3 = input.nextInt();
+        System.out.println("Türkçe Notu giriniz : ");
+        not4 = input.nextInt();
+        System.out.println("Tarih Notu giriniz : ");
+        not5 = input.nextInt();
+        System.out.println("Müzik Notu giriniz : ");
+        not6 = input.nextInt();
+        ortalama = (not1 + not2 + not3 + not4 + not5 + not6) / 6;
+        System.out.println("Ortalamaniz : " + ortalama);
+    }
+}
